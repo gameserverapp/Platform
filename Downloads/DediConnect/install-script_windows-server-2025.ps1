0267a47297ee4ebd163dcdbd899fa012f24297b7
Max Vaessen <maxvaessen@gmail.com>
2026-03-31 17:47:51 +0200

    add windows 2025 install script

---
+++ Downloads/DediConnect/install-script_windows-server-2025.ps1
@@ -0,0 +1,54 @@
+# ########################################################
+# Copyright © 2023 GameServerApp.com. All rights reserved.
+# ########################################################
+
+Set-ExecutionPolicy Bypass -Scope Process -force
+Set-ExecutionPolicy Bypass -force
+
+function Test-Administrator
+{
+    [OutputType([bool])]
+    param()
+    process {
+        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+    }
+}
+
+if(-not (Test-Administrator))
+{
+    Write-Error "This script must be executed as Administrator.";
+    pause;
+    exit 1;
+}
+
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~
+
+Set-Service -Name sshd -StartupType Automatic
+
+Get-NetFirewallRule -Name *ssh*
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+
+net start sshd
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+C:\ProgramData\chocolatey\bin\choco.exe install -y python --version=3.14.3
+C:\ProgramData\chocolatey\bin\choco.exe install -y nssm
+
+Install-WindowsFeature -Name Web-Mgmt-Tools
+Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature
+Install-WindowsFeature WAS
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Install-Module PSWindowsUpdate -confirm:$false -force
+Get-WindowsUpdate -Install -acceptall -IgnoreReboot
+
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+
+.\install-docker-ce.ps1 -NoRestart
+
+echo "" >> C:\Users\Administrator\.gsa-script-installed
+
+Restart-Computer -Force
